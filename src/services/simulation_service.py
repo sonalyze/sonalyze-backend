40c8707b892834f4e5f09b6b912f562fa241e22b
Jessica Trojna <jeffsemai@web.de>
2025-05-27 17:36:10 +0200

    FEAT:37 now can simulate multiple microphones/speakers

--- src/services/simulation_service.py
+++ src/services/simulation_service.py
@@ -1,6 +1,7 @@
 from api.models.room_scene import RestRoomScene
 from api.models.simulation import Simulation
 import pyroomacoustics as pra
+import numpy as np
 
 
 def simulate_room(room_scene: RestRoomScene) -> Simulation | None:
@@ -9,31 +10,33 @@ def simulate_room(room_scene: RestRoomScene) -> Simulation | None:
         room_scene.dimensions.depth,
         room_scene.dimensions.height,
     ]  # Breite, Tiefe, Höhe
+
     room = pra.ShoeBox(
         room_dim, fs=16000, absorption=0.3, max_order=10
     )  # Standardabsorption mit genauen Materialien ersetzen
-    room_scene.speakers[0].x
 
-    source_location = [
-        room_scene.speakers[0].x,
-        room_scene.speakers[0].y,
-        room_scene.speakers[0].z,
-    ]
-    room.add_source(source_location)
+    for speaker in room_scene.speakers:
+        source_pos = [speaker.x, speaker.y, speaker.z]
+        room.add_source(source_pos)
 
-    room_scene.microphones[0].x
-    mic_location = [
-        room_scene.microphones[0].x,
-        room_scene.microphones[0].y,
-        room_scene.microphones[0].z,
-    ]
-    room.add_microphone(mic_location)
+    mic_positions = np.array(
+        [
+            [m.x for m in room_scene.microphones],
+            [m.y for m in room_scene.microphones],
+            [m.z for m in room_scene.microphones],
+        ]
+    )
+
+    room.add_microphone_array(pra.MicrophoneArray(mic_positions, fs=16000))
 
     room.compute_rir()
     if room.rir is None:
         raise RuntimeError(
             "Raum konnte nicht simmuliert werden – eventuell ist das Setup fehlerhaft."
         )
-    rir = room.rir[0][0]
-    # Verarbeiten der Impulsantwort einfügen
+    for mic_index, mic_rirs in enumerate(room.rir):
+        for source_index, rir in enumerate(mic_rirs):
+            print(rir)
+            # rir ist Impulsantwort die weiterverarbeitet werden muss
+            # Verarbeiten der Impulsantwort einfügen
     return None

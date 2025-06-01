53e6417f44135936c8f8efa3068665d4d08f3db1
Jessica Trojna <jeffsemai@web.de>
2025-06-01 17:31:10 +0200

    FEAT:24 type changes

--- src/services/get_material.py
+++ src/services/get_material.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from dotenv import dotenv_values
+from typing import Any
 
 # DB-Verbindung
 env = dotenv_values("../../.env")
@@ -7,13 +8,13 @@ uri = f"mongodb://{env.get('DB_CONNECTION_STRING', '')}"
 if not uri:
     raise ValueError("DB_CONNECTION_STRING is empty or missing in .env")
 
-client: MongoClient
+client: MongoClient[Any]
 client = MongoClient(uri)
 db = client["test_database"]
 collection = db["materials"]
 
 
-def get_material_json_from_db(name: str):
+def get_material_json_from_db(name: str) -> dict[str, list[float]]:
     # Suche alle Materialien mit Beschreibung, die 'name' enthält (case-insensitive)
     result = collection.find_one({"description": {"$regex": name, "$options": "i"}})
 

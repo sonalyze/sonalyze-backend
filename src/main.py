c1209a383c8f765a0889e3c58ed01c0cb5fd2a86
Julian <thejulianjuko@gmail.com>
2025-06-06 17:46:12 +0200

    updated cors, removed /api prefix, added swagger desc

--- src/main.py
+++ src/main.py
@@ -6,11 +6,18 @@ from sio.socketio_server import sio_app
 from dotenv import load_dotenv
 
 load_dotenv()
-app = FastAPI()
+app = FastAPI(
+    title="Sonalyze API",
+    description="This is the API for Sonalyze.",
+    version="1.0.0",
+    openapi_url="/openapi.json",
+    docs_url="/docs"
+)
 
 origins = [
-    "http://localhost:3000",
-    "https://aal.todo",
+    "http://localhost:8081",
+    "https://sonalyze.de",
+    "https://dev.sonalyze.de",
 ]
 app.add_middleware(
     CORSMiddleware,
@@ -22,4 +29,4 @@ app.add_middleware(
 
 app.mount("/socket.io", sio_app)
 
-app.include_router(api_router.router, prefix="/api")
+app.include_router(api_router.router)
